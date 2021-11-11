1d0271106180208bf1f8bbc8dba5dde5d6641106
John Cerreta <john.cerreta@aa.com>
2021-11-11 16:08:29 -0600

    updating revenue/expense sql

--- usecase#/SQL_1.sql
+++ usecase#/SQL_1.sql
@@ -1,39 +1,135 @@
-Select "YEAR", "Project", COALESCE(EXPENSES,0) AS EXPENSES, COALESCE(REVENUE,0) AS REVENUE
+Select "YEAR", C."Project", "Project Name", Expenses, Revenue
 from
+
+
+
 (
+
+
+
+Select "YEAR", "Project", COALESCE(EXPENSES,0) as Expenses, COALESCE(REVENUE,0) as Revenue
+from
+
+
+
+(
+
 SELECT "Project"
 , (sum("Debit") - sum("Credit")) as Expenses
 , extract(year from to_timestamp("Posted Dt.")) as Year
 FROM "PROD_CTF_RAW"."SRC_SPREADSHEET"."2020_GL_FUNDRAISING_EXPENSES"
 group by "Project", Year
+
+
+
+
 UNION
+
+
+
 SELECT "Project"
 , (sum("Debit") - sum("Credit")) as Expenses
 , extract(year from to_timestamp("Posted Dt.")) as Year
 FROM "PROD_CTF_RAW"."SRC_SPREADSHEET"."2019_GL_FUNDRAISING_EXPENSES"
 group by "Project", Year
+
 UNION
+
+
+
 SELECT "Project"
 , (sum(coalesce("Debit",0)) - sum(coalesce("Credit",0))) as Expenses
 , extract(year from to_timestamp("Posted Dt.")) as Year
 FROM "PROD_CTF_RAW"."SRC_SPREADSHEET"."2018_GL_FUNDRAISING_EXPENSES"
 group by "Project", Year
+
 UNION
+
+
+
 SELECT "Project"
 , (sum(coalesce("Debit",0)) - sum(coalesce("Credit",0))) as Expenses
 , extract(year from to_timestamp("Posted Dt.")) as Year
 FROM "PROD_CTF_RAW"."SRC_SPREADSHEET"."2021_GL_FUNDRAISING_EXPENSES"
 group by "Project", Year
+
 ) a
+
 left join
+
 (
+
 SELECT year(CLOSEDATE) AS CLOSEDATE, INTACCT_PROJECT_CODE__C, SUM(coalesce(AMOUNT,0)) as REVENUE
 FROM "PROD_CTF_RAW"."SRC_SF"."CTF_SF_OPPORTUNITY"
 WHERE year(CLOSEDATE) in (2021,2020,2019,2018)
 AND ISWON = 1
 GROUP BY YEAR(CLOSEDATE), INTACCT_PROJECT_CODE__C
+
 ) B
+
 on A.YEAR = B.CLOSEDATE
 and "Project" = "INTACCT_PROJECT_CODE__C"
 group by "YEAR","Project", "EXPENSES", "REVENUE"
-order by "YEAR", "Project"
+) C
+
+
+
+Left join
+
+
+
+(Select * from
+
+
+
+(
+
+SELECT "Project"
+, "Project Name"
+FROM "PROD_CTF_RAW"."SRC_SPREADSHEET"."2020_GL_FUNDRAISING_EXPENSES"
+
+
+
+
+UNION
+
+
+
+SELECT "Project"
+, "Project Name"
+FROM "PROD_CTF_RAW"."SRC_SPREADSHEET"."2019_GL_FUNDRAISING_EXPENSES"
+
+
+
+
+UNION
+
+
+
+SELECT "Project"
+, "Project Name"
+FROM "PROD_CTF_RAW"."SRC_SPREADSHEET"."2018_GL_FUNDRAISING_EXPENSES"
+
+
+
+
+UNION
+
+
+
+SELECT "Project"
+, "Project Name"
+
+
+
+FROM "PROD_CTF_RAW"."SRC_SPREADSHEET"."2021_GL_FUNDRAISING_EXPENSES"
+
+
+
+)
+) D
+
+
+
+on C."Project" = D."Project"
+order by "YEAR", C."Project"
